ad896f119be9cd0b58b3cb1c1498f878473188f2
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-03 13:39:32 -0400

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,80 @@
+#The Insert Server Download Asset System..
+import requests
+from flask import Flask,request
+#The Insert Server Download Asset System..
+
+app = Flask(__name__)
+@app.route('/')
+
+def hello_world():
+    theid = None
+    asset_type = None
+    myQuery = getParams(str(request.url))
+    if (myQuery!=None):
+        idq = str(myQuery[0])
+        tyq = None
+        if (len(myQuery)>1):
+            tyq = str(myQuery[1])
+        ##endif
+        if (idq!=None):
+            if (tyq==None or tyq=='type=model'):
+                theid = int(idq.split('=')[1])
+                asset_type = 'rbxm'
+                if (theid!=None):
+                    insertserver.downloadAsset(theid)
+                ##endif
+            ##endif
+        ##endif
+    ##endif
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Insert Cloud API - Asset Downloader</title>
+        <link rel='icon' href='/images/favicon.ico'/>
+        <link rel='stylesheet' href='/css/styles-main.css'/>
+    </head>
+    <body>
+        <h1 class='red2'>InsertAPI Server: </h1>
+        <h2 class='red1'>Download Asset Request Recieved.</h2>
+        <p class='red1'>Asset Location: <a href='/assets/v1/"""+str(theid)+"""'>/assets/v1/"""+str(theid)+"""</a></p>
+    </body>
+</html>
+"""
+##end
+def getParams(url):
+    if (len(url.split('?'))>1):
+        query = url.split('?')[1]
+        params = query.split('&')
+        return params
+    ##endif
+##end
+
+class insertserver:
+    def downloadAsset(assetid):
+        url = 'https://assetdelivery.roblox.com/v1/asset/?id='+str(assetid)
+        REQUEST = requests.get(url)
+        if (REQUEST.status_code<400):
+            rawData = REQUEST.content
+            asset = open('InsertCloudAPI/assets/v1/'+str(assetid), "w")
+            asset.write(str(rawData))
+            return asset
+        else:
+            print('REQUEST_ERROR: '+str(REQUEST.status_code))
+            return {'STATUS_CODE':REQUEST.status_code,'ERROR_MESSAGE':'REQUEST_ERROR: '+str(REQUEST.status_code)}
+        ##endif
+    ##end
+    def downloadAudio(assetid):
+        url = 'https://api.hyra.io/audio/'+str(assetid)
+        REQUEST = requests.get(url)
+        if (REQUEST.status_code<400):
+            rawData = REQUEST.content
+            asset = open('InsertCloudAPI/assets/v1/mp3/'+str(assetid)+".mp3", "w")
+            asset.write(str(rawData))
+            return asset
+        else:
+            print('REQUEST_ERROR: '+str(REQUEST.status_code))
+            return {'STATUS_CODE':REQUEST.status_code,'ERROR_MESSAGE':'REQUEST_ERROR: '+str(REQUEST.status_code)}
+        ##endif
+    ##end
+##end
